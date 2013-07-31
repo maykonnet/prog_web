b84d454d847da8bfc67d026345950f7626b17567
Laislla <laislla.ramos@gmail.com>
2013-07-31 17:58:58 -0400

    Novas funcoes no editar

--- application/controllers/UsuarioController.php
+++ application/controllers/UsuarioController.php
@@ -12,19 +12,45 @@ class UsuarioController extends Zend_Controller_Action
     {
     	$SessionNamespace = new Zend_Session_Namespace('Usuario');
     	$SessionNamespace->usuario_id = '2';
-    	if(empty($SessionNamespace->usuario_id)){
+    	if(!empty($SessionNamespace->usuario_id)){
     		$this->_helper->redirector('usuario','editar');
     	}
     }
 
     public function loginAction()
     {
-        // action body
+    	$request = $this->getRequest();
+    	$model_usuario = new Application_Model_Usuario();
+    	 
+    	if ($request->isPost()) {
+    		$formDados = $request->getPost();
+    		if ($formDados) {
+    			$resultado = false;
+    			try {
+    				$usuario_dados = $model_usuario->login($formDados);
+			    	$SessionNamespace = new Zend_Session_Namespace('Usuario');
+			    	$SessionNamespace->usuario_id = $usuario_dados['usuario_id'];
+			    	$SessionNamespace->usuario_nome = $usuario_dados['usuario_nome'];
+			    	$SessionNamespace->usuario_sexo = $usuario_dados['usuario_sexo'];
+			    	$SessionNamespace->usuario_login = $usuario_dados['usuario_login'];			
+    				
+    			} catch (Exception $ex) {
+    				$this->view->erro = $ex->getMessage();
+    			}
+    			 
+    			$this->_helper->redirector('index','index');
+    		}
+    	}
     }
 
     public function logoutAction()
-    {
-        // action body
+    {		
+		if (Zend_Session:: namespaceIsset('Usuario')) {
+	        $SessionAluno = new Zend_Session_Namespace('Usuario');
+	        $SessionAluno->unsetAll('');
+        }
+
+        $this->_helper->redirector('index', 'index');
     }
 
     public function cadastroAction()
@@ -54,7 +80,17 @@ class UsuarioController extends Zend_Controller_Action
     	$request = $this->getRequest();
     	$model_usuario = new Application_Model_Usuario();
     	
-    	if ($request->isPost()) {
+    	$SessionNamespace = new Zend_Session_Namespace('Usuario');
+    	$usuario_id = $SessionNamespace->usuario_id;
+    	
+    	$dados_usuario = $model_usuario->getDadosUsuario($usuario_id);
+    	$this->view->usuario_id = $usuario_id;
+    	$this->view->usuario_nome = $dados_usuario['usuario_nome'];
+    	$this->view->usuario_sexo = $dados_usuario['usuario_sexo'];
+    	$this->view->usuario_login = $dados_usuario['usuario_login'];
+    	$this->view->usuario_senha = $dados_usuario['usuario_senha'];
+    	    	
+    	if($request->isPost()) {
     		$formDados = $request->getPost();
     		echo '<pre>';
     		print_r($formDados);
--- application/models/Usuario.php
+++ application/models/Usuario.php
@@ -14,6 +14,55 @@ class Application_Model_Usuario
 		
 		$_db_usuario->insert($dados_banco);
 	}
+	
+	public function editarUsuario($dados)
+	{
+		$_db_usuario = new Application_Model_DbTable_Usuario();
+		
+		$dados_banco = array('usuario_nome' => $dados['nome'],
+							'usuario_sexo' => $dados['sexo'],
+							'usuario_login' => $dados['login_C'],
+							'usuario_senha' => md5($dados['senha_C']));
+		
+		$_db_usuario->update($dados_banco, 'usuario_id = '.$dados['usuario_id']);
+	}
+	
+	public function getDadosUsuario($usuario_id)
+	{
+		$_db_usuario = new Application_Model_DbTable_Usuario();
 
+		$dados_banco = $_db_usuario->fetchRow($usuario_id);
+		
+		$dados = array('usuario_nome' => $dados_banco['usuario_nome'],
+							'usuario_sexo' => $dados_banco['usuario_sexo'],
+							'usuario_login' => $dados_banco['usuario_login'],
+							'usuario_senha' => $dados_banco['usuario_senha']);
+		
+		return $dados;			
+	}
+
+	public function login($dados_login)
+	{
+		$_db_usuario = new Application_Model_DbTable_Usuario();
+		$login = $dados_login['login'];
+		$senha = $dados_login['senha'];
+	
+		$dados_banco = $_db_usuario->fetchRow("usuario_login = '".$login."' and usuario_senha = '".$senha."'");
+	
+		$dados = array('usuario_id' => $dados_banco['usuario_id'],
+				'usuario_nome' => $dados_banco['usuario_nome'],
+				'usuario_sexo' => $dados_banco['usuario_sexo'],
+				'usuario_login' => $dados_banco['usuario_login'],
+				'usuario_senha' => $dados_banco['usuario_senha']);
+	
+		return $dados;
+	}
+	
+	public function excluirUsuario($usuario_id)
+	{
+		$_db_usuario = new Application_Model_DbTable_Usuario();
+		$_db_usuario->delete('usuario_id ='.$usuario_id);		
+	}
+	
 }
 
